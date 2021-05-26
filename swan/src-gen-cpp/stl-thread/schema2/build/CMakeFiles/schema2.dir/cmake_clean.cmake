843dbf62b676987a16b50edd5fe49d08dedb94be
lemairen <lemairen@u-un00301115>
2021-05-26 16:09:33 +0200

    Ajout scheam2 Geo

---
+++ swan/src-gen-cpp/stl-thread/schema2/build/CMakeFiles/schema2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/schema2.dir/Schema2.cc.o"
+  "schema2"
+  "schema2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/schema2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
